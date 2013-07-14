8b0560474c13a930013581ee56b91eefa7735ce4
cipher <gmgateway@gmail.com>
2013-07-14 17:05:01 +0900

    Initial Commit

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Compass configuration file.
+
+# We also support plugins and frameworks, please read the docs http://docs.mixture.io/preprocessors#compass
+
+# Important! change the paths below to match your project setup
+css_dir = "assets/styles" # update to the path of your css files.
+sass_dir = "scss" # update to the path of your sass files.
+images_dir = "assets/images" # update to the path of your image files.
+javascripts_dir = "assets/js" # update to the path of your script files.
+fonts_dir = "assets/fonts"
+
+line_comments = false # if debugging (or using Mixture chrome extension - set this to true)
+cache = true
+color_output = false # required for Mixture
+preferred_syntax = :scss
